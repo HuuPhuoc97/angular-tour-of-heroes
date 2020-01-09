0ecff8620016f47f684a8cd8ab9ce4957dfbabfd
Phuoc <phuoc.lh@3si.vn>
2020-01-09 17:00:56 +0700

    route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
-  constructor() { }
+  // heroes = HEROES;
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
   ngOnInit() {
+    this.getHeroes();
   }
 
+  // ở đây ì selectedHero gán bằng hero nên khi thay đổi selectedHero thì this.hero khai báo ở đầu class cũng change
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.heroes);
+  }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
